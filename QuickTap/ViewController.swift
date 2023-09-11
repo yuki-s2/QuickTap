1b12cc1dc960227b1bb4157cb86350fd72d919ca
Yuki Honda <dahlia.15s2@gmail.com>
2023-09-11 19:38:36 +0800

    Initial Commit

---
+++ QuickTap/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  QuickTap
+//
+//  Created by Yuki Honda on 2023/09/11.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
